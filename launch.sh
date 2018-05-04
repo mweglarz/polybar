f043aec30701059faf86ba17979100b8223d91a3
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-05-04 23:52:27 +0200

    Init

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# terminate already running instances
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+polybar bar1 &
+polybar bar2 &
+
+echo "Bars launched..."
+
