7236f751c481f818b36bb64d853e18503ae8157c
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-05-05 23:40:24 +0200

    Working config

--- launch.sh
+++ launch.sh
@@ -8,7 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar bottomR 2>/dev/null &
 polybar bottomL 2>/dev/null &
-#polybar topR 2>/dev/null &
+polybar topL 2>/dev/null &
+polybar topR 2>/dev/null &
 
 echo "Bars launched..."
 
