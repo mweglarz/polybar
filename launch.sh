ad2e8a408c9bbd7f1f0a8c396d57b1e6fd045757
Marcin Weglarz <marcin.weglarz01@gmail.com>
2018-05-05 18:03:02 +0200

    Update

--- launch.sh
+++ launch.sh
@@ -6,8 +6,9 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar bar1 &
-# polybar bar2 &
+polybar bottomR 2>/dev/null &
+polybar bottomL 2>/dev/null &
+#polybar topR 2>/dev/null &
 
 echo "Bars launched..."
 
